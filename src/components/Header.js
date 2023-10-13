1ac6d4547016ab120a63a6839476817fe78f1a9c
uvaleks <uvaleks@protonmail.com>
2023-10-13 15:49:07 +0300

    feat: reimplemented singup, login screens & mobile menu design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import logoPath from '../images/logo.svg';
+import { useState } from 'react';
 
 function Header() {
+  const [isMobileMenuOpened, setMobileMenuOpened] = useState(false);
+
+  function handleMenuClick() {
+    setMobileMenuOpened(!isMobileMenuOpened);
+  }
+
   return (
     <header className="header">
         <img className="header__logo" src={logoPath} alt="Логотип Место"/>
+        <div className={`header__menu ${isMobileMenuOpened ? 'header__menu_opened' : ''}`}>
+          <a className="header__menu-email">uvaleks@mail.ru</a>
+          <a className="header__menu-button">Войти</a>
+        </div>
+        <a onClick={handleMenuClick} className={`header__menu-burger ${isMobileMenuOpened ? 'header__menu-burger_icon_close' : 'header__menu-burger_icon_burger'}`}></a>
     </header>
   );
 }
---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,20 @@
+import okTooltipStatus from '../images/ok-tooltip-status.svg';
+import errorTooltipStatus from '../images/error-tooltip-status.svg';
+
+function InfoTooltip({ tooltipStatus, isOpen, onClose }) {
+  return (
+    <div className={`popup ${isOpen ? 'popup_opened' : ''}`}>
+          <div className="tooltip">
+              <img className="tooltip__status-icon" src={(tooltipStatus === 'ok') ? okTooltipStatus : errorTooltipStatus} />
+              <h2 className="tooltip__title">{(tooltipStatus === 'ok') ? "Вы успешно зарегистрировались!" : `Что-то пошло не так! Попробуйте ещё раз.`}</h2>
+              <button className="popup__close-button" type="button" aria-label="Закрыть" onClick={onClose}></button>
+          </div>
+    </div>
+  );
+}
+
+export default InfoTooltip;
+
+
+
+
